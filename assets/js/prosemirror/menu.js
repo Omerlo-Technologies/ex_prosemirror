64a12f283095c61fe12def765a6af9e571eef684
Alexandre Lepretre <alexandre.lprtr@gmail.com>
2021-05-28 10:07:23 -0400

    Add jsconfig.json
    Improve code completion / validation for developers of this project

--- assets/js/prosemirror/menu.js
+++ assets/js/prosemirror/menu.js
@@ -2,20 +2,28 @@ import {toggleMark, setBlockType, wrapIn} from 'prosemirror-commands';
 import { Plugin } from 'prosemirror-state';
 import MenuView from './MenuView';
 
+/**
+ * @param {any[]} items
+ */
 function menuPlugin(items) {
   return new Plugin({
     view(editorView) {
-      let menuView = new MenuView(items, editorView)
-      editorView.dom.parentNode.insertBefore(menuView.dom, editorView.dom)
-      return menuView
+      let menuView = new MenuView(items, editorView);
+      editorView.dom.parentNode.insertBefore(menuView.dom, editorView.dom);
+      return menuView;
     }
-  })
+  });
 }
 
-// Helper function to create menu icons
+/**
+ * Helper function to create menu icons
+ *
+ * @param {string} text
+ * @param {string} name
+ */
 function icon(text, name) {
-  let span = document.createElement("span");
-  span.className = "menuicon " + name;
+  let span = document.createElement('span');
+  span.className = 'menuicon ' + name;
   span.title = name;
   span.textContent = text;
   return span;
@@ -23,16 +31,16 @@ function icon(text, name) {
 
 export default ({ schema }) => {
   return menuPlugin([
-    {command: toggleMark(schema.marks.strong), dom: icon("B", "strong")},
-    {command: toggleMark(schema.marks.em), dom: icon("i", "em")},
+    {command: toggleMark(schema.marks.strong), dom: icon('B', 'strong')},
+    {command: toggleMark(schema.marks.em), dom: icon('i', 'em')},
     {command: setBlockType(schema.nodes.p), dom: icon('p', 'paragraph')},
-    {command: setBlockType(schema.nodes.h1), dom: icon("h1", "h1")},
-    {command: setBlockType(schema.nodes.h2), dom: icon("h2", "h2")},
-    {command: setBlockType(schema.nodes.h3), dom: icon("h3", "h3")},
-    {command: setBlockType(schema.nodes.h4), dom: icon("h4", "h4")},
-    {command: setBlockType(schema.nodes.h5), dom: icon("h5", "h5")},
-    {command: setBlockType(schema.nodes.h6), dom: icon("h6", "h6")},
+    {command: setBlockType(schema.nodes.h1), dom: icon('h1', 'h1')},
+    {command: setBlockType(schema.nodes.h2), dom: icon('h2', 'h2')},
+    {command: setBlockType(schema.nodes.h3), dom: icon('h3', 'h3')},
+    {command: setBlockType(schema.nodes.h4), dom: icon('h4', 'h4')},
+    {command: setBlockType(schema.nodes.h5), dom: icon('h5', 'h5')},
+    {command: setBlockType(schema.nodes.h6), dom: icon('h6', 'h6')},
 
-    {command: wrapIn(schema.nodes.blockquote), dom: icon(">", "blockquote")}
-  ])
+    {command: wrapIn(schema.nodes.blockquote), dom: icon('>', 'blockquote')}
+  ]);
 };
